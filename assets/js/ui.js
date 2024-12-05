95a734d3198cbcf1fe86a27b58f46db928cc73ed
DESKTOP-95SPK72\iloveisyou_lenovo <iloveisyou@hanmir.com>
2024-12-05 18:14:03 +0900

    Update 2024-12-05T09:13:37.161Z

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -1,6 +1,61 @@
-(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-"use strict";document.addEventListener("DOMContentLoaded",function(){console.log("스크립트")});
+document.addEventListener('DOMContentLoaded',()=>{
+    let today = new Date();
+    console.log('script_' + today.getHours() + today.getMinutes() + today.getSeconds() );
+});
 
-},{}]},{},[1]);
+function uiRUI(ruiObj=false) {
+    const _obj = ruiObj;
+    const _rui = (_obj+'').split('.');
+    console.log('uiRUI? '+_rui);
+    
+    if(_rui[0]!=='Rui') { return false; }  
+    // console.log(_obj);
 
-//# sourceMappingURL=ui.js.map
+    // rui > form
+    if(_rui[2]==='form') {
+        const j_this = $(`#${_obj.id}`);
+
+        // focus 체크
+        j_this.on('focusin', function() { j_this.parents('.fm_text').addClass('is-focus'); });
+        j_this.on('focusout', function() { j_this.parents('.fm_text').hasClass('is-focus') && j_this.parents('.fm_text').removeClass('is-focus'); });
+        
+        // rui > form > input type=textbox
+        if(_rui[3].split(' ')[0]==='LTextBox') {
+            
+            // value 체크
+            j_this.find('input:not([type=hidden])').on('change paste keyup', function() {
+                if(j_this.find('input:not([type=hidden])').val() !== '' && j_this.find('input:not([type=hidden])').val().length > 0){
+                    j_this.parent('.fm_text').addClass('is-value');
+                }else {
+                    j_this.parents('.fm_text').hasClass('is-value') && j_this.parents('.fm_text').removeClass('is-value');
+                }
+            });
+            
+            // rui > form > input type=textbox > auto complete
+            if(_obj.autoComplete){
+                const j_option = $(`#${_obj.optionDivEl.id}`);
+                j_option.css('width', j_this.width() + 'px'); // 옵션 박스 넓이 재설정
+            }
+        }
+
+        // rui > form > input type=databox
+        if(_rui[3].split(' ')[0]==='LDateBox') {
+            const j_option = $(`#${_obj.calendar.id}`);
+            j_option.css('transform', `translateX(${j_this.outerWidth(true) - j_option.outerWidth(true)}px)`); // 날짜선택 박스 위치 재설정
+        }
+        
+        // rui > form > input type=checkbox
+        if(_rui[3].split(' ')[0]==='LCheckBox') {
+        }
+        
+        // rui > form > radio type=radio
+        if(_rui[3].split(' ')[0]==='LRadio') {
+        }
+        
+        // rui > form > combo type=default
+        if(_rui[3].split(' ')[0]==='LCombo') {
+            const j_option = $(`#${_obj.optionDivEl.id}`);
+            j_option.css('width', j_this.width() + 'px'); // 옵션 박스 넓이 재설정
+        }
+    }
+}
