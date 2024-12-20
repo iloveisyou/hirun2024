4c2a84ec45ccc3f0969d019760b9d4b23ca65e12
DESKTOP-95SPK72\iloveisyou_lenovo <iloveisyou@hanmir.com>
2024-12-20 19:04:49 +0900

    Update 2024-12-20T10:04:06.141Z

--- js/ui.js
+++ js/ui.js
@@ -1,9 +1,12 @@
+'use strict';
+
 document.addEventListener('DOMContentLoaded',()=>{
     let today = new Date();
     console.log('script_' + today.getHours() + today.getMinutes() + today.getSeconds() );
 });
 
 var jsonURL = (window.location.host.length > 0) ? '../json/mockup.json' : 'https://iloveisyou.github.io/hirun2024/json/mockup.json';
+
 
 
 /*********************************************************/
@@ -178,95 +181,115 @@ function fmWrapIdChange(_jthis, _rui = false) {
 
     $.each(jthis.find('input[type="checkbox"], input[type="radio"]'), function(i,el){
         $(el).parent('.L-form-field').attr('id') && $(el).parent('.L-form-field').attr('id', `${$(el).parent('.L-form-field').attr('id')}_wrap`);
-    })
+    });
     
 }
 
 
 /*********************************************************/
+/**********************  공통 유틸  **********************/
+/*********************************************************/
+// 인뎃스 구하기
+const index=(element)=> [].indexOf.call(element.parentElement.children, element);
+
+
+/*********************************************************/
 /**********************  공통 제어  **********************/
 /*********************************************************/
-let ui = (function(window, document, $) {
+var uiux = (function(window, document, $) {
     'use strict';
 
-    function gnb() {
-        let jthis = $('.gnb');
-        let slide = jthis.find('.slider');
+    // 공통 변수 설정
+    let _wrap = document.querySelector('.hirunWrap'); // 전체래퍼
+    let stimer, userMouse = true, userMouseTimer, userMouseStatus; // 로그인섹션 변수
+    window.addEventListener('DOMContentLoaded', function(){
+        _wrap = document.querySelector('.hirunWrap');
+    });
 
-        gnbInit();
-        // jthis.off('mouseenter').on('mouseenter', function(ev){
-            // !$(this).hasClass('is-active') && $(this).addClass('is-active');
-            console.log('2222222')
+    function scroll() { // 스크롤 이벤트 공통
+        window.addEventListener('scroll', function(e) {
+            if(window.scrollY > 20) {
+                !_wrap.classList.contains('sc-headerSmall') && _wrap.classList.add('sc-headerSmall'); 
+            }else {
+                _wrap.classList.contains('sc-headerSmall') && _wrap.classList.remove('sc-headerSmall'); 
+            }
+        });
+    }
 
-            // $(this).find('li').each(function(i,li){
-        jthis.find('li').each(function(i,li){
+    function integratedSearch() { // 헤더 통합검색
+        let _this = document.querySelector('.cp_integratedSearch');
+        let _trigger = document.querySelector('.bt_headerUtil.integratedSearch');
 
-            $(li).off('mouseover').on('mouseover', function(liEv){
-
-
-                $(this).siblings('.is-active').removeClass('is-active');
-                !$(this).hasClass('is-active') && $(this).addClass('is-active');
-                !jthis.hasClass('is-active') && jthis.addClass('is-active');
-
-                
-                gnbSlider(i,this);
-                
-                $(this).find('dl>div').each(function(j,div){
-                    $(div).off('mouseover').on('mouseover', function(divEv){
-                        !$(this).hasClass('is-active') && $(this).addClass('is-active');
-                        $(this).find('dd').each(function(k, dd) {
-                            $(dd).off('mouseover').on('mouseover', function(ddEv){
-                                $(this) !== $(this).parents('div').find('dd.is-active') && $(this).parents('div').find('dd.is-active').removeClass('is-active')
-                                !$(this).hasClass('is-active') && $(this).addClass('is-active');
-                            });
-                            $(dd).off('mouseout').on('mouseout', function(ddEv){
-                                $(this).hasClass('is-active') && $(this).removeClass('is-active');
-                                gnbInit();
-                            });
-                        });
-                    });
-                    $(div).off('mouseout').on('mouseout', function(liEv){
-                        $(this).hasClass('is-active') && $(this).removeClass('is-active');
-                    });
-                });
+        _trigger.addEventListener('click', function(e) {
+            e.preventDefault();
+            !_this.classList.contains('is-active') && _this.classList.add('is-active');
+        })
+        _this.querySelector('.bt_ntegratedSearchClose').addEventListener('click', function() {
+            _this.classList.contains('is-active') && _this.classList.remove('is-active');
+        });
+        _this.querySelectorAll('.keyword dd').forEach(function(el,i) {
+            el.addEventListener('mouseover', function(e) {
+                _this.querySelector('input').value = `${e.currentTarget.innerHTML}을 검색해볼까?`;
             });
-            $(li).off('mouseout').on('mouseout', function(liEv){
-                $(this).hasClass('is-active') && $(this).removeClass('is-active');
-                jthis.hasClass('is-active') && jthis.removeClass('is-active');
-                gnbInit();
+            el.addEventListener('mouseout', function(e) {
+                _this.querySelector('input').value = ``;
             });
         });
-
-        // });
-        // jthis.off('mouseleave').on('mouseleave', function(ev){
-        //     $(this).hasClass('is-active') && $(this).removeClass('is-active');
-        // });
-
-        function gnbInit() {
-            jthis.find('li[data-active="true"]:not(.is-active)').addClass('is-active');
-            jthis.find('dd[data-active="true"]:not(.is-active)').addClass('is-active');
-            gnbSlider(jthis.find('li').index(jthis.find('li.is-active')), jthis.find('li.is-active'));
-            // console.log(jthis.find('li').index(jthis.find('li.is-active')));
-        }
-        function gnbSlider(i,el) {
+    }
+    
+    function gnb() { // gnb 이벤트
+        const _this = document.querySelector('.gnb');
+        const _slide = _this.querySelector('.slider');
+        const gnbInit=()=> {
+            _this.querySelector('li[data-active="true"]:not(.is-active)')?.classList.add('is-active');
+            _this.querySelector('dd[data-active="true"]:not(.is-active)')?.classList.add('is-active');
+            gnbSlider(index(_this.querySelector('li.is-active')), _this.querySelector('li.is-active'));
+        };
+        const gnbSlider=(idx,target)=> {
             let slideX = 0;
-            $(el).parents('ul').find('li').each(function(j,item){
-                if(j < i) {
-                    slideX += parseInt($(item).outerWidth()) + 10;
-                }
+            target.closest('ul').querySelectorAll('li').forEach(function(el,i){
+                if(i < idx) { slideX += parseInt(el.getBoundingClientRect().width) + 10; }
             });
-            slide.css({'transform':`translateX(${slideX}px)`, 'width':`${$(el).outerWidth()}px`});
-        }
-    }
-    function tab() {
-        console.log('tab')
+            _slide.style.transform = `translateX(${slideX}px)`;
+            _slide.style.width = `${target.getBoundingClientRect().width}px`;
+        };
+
+        gnbInit();
+        _this.querySelectorAll('li').forEach(function(el,i){
+            el.addEventListener('mouseover', function(e){
+                gnbSlider(i,e.currentTarget);
+            });
+            el.addEventListener('mouseout', function(e){
+                gnbInit();
+                console.log('aaa')
+            });
+        });
     }
 
-    let stimer, userMouse = true, userMouseTimer, userMouseStatus;
-    function sectionTimer(time,clear,callback) {
+
+    function tab() { // 탭 이벤트
+        console.log('tab');
+    }
+
+    function sectionTimer(time,clear,callback) { // 로그인 섹션 이벤트
         let currentTime = time;
-        let target = '.sectionTimer';
+        const target = document.querySelector('.sectionTimer');
         if(clear == true) { clearInterval(stimer); }
+        const counterInit=()=> { target.innerHTML = ((~~(currentTime/60)) < 10 ? ('0'+~~(currentTime/60)) : ~~(currentTime/60)) + ' : ' + ((~~(currentTime%60)) < 10 ? ('0'+~~(currentTime%60)) : ~~(currentTime%60)); };
+        const counter=()=> {
+            currentTime=currentTime-1;
+            target.innerHTML = ((~~(currentTime/60)) < 10 ? ('0'+~~(currentTime/60)) : ~~(currentTime/60)) + ' : ' + ((~~(currentTime%60)) < 10 ? ('0'+~~(currentTime%60)) : ~~(currentTime%60));
+            if(currentTime <= 10) { 
+                !target.classList.contains('is-warning') && target.classList.add('is-warning');
+            }else{
+                target.classList.contains('is-warning') && target.classList.remove('is-warning');
+            }
+            if(currentTime == 0) { clearInterval(stimer); setTimeout(()=>alert('로그아웃되었습니다.'),100) }
+        };
+        const stimerStart=()=> { 
+            clearInterval(stimer);
+            stimer = setInterval(counter,1000); 
+        };
 
         counterInit();
         setInterval(function(){
@@ -276,49 +299,74 @@ let ui = (function(window, document, $) {
                 userMouse=false;
             }
         },1000);
-        
-        $(window).off().on('mousemove', function(){
+        window.addEventListener('mousemove', function(){
             clearTimeout(userMouseTimer);
             userMouse = true;
         });
+    }
 
-        function counterInit() { $(target).text(((~~(currentTime/60)) < 10 ? ('0'+~~(currentTime/60)) : ~~(currentTime/60)) + ' : ' + ((~~(currentTime%60)) < 10 ? ('0'+~~(currentTime%60)) : ~~(currentTime%60))); }
-        function counter() {
-            currentTime=currentTime-1;
-            $(target).text(((~~(currentTime/60)) < 10 ? ('0'+~~(currentTime/60)) : ~~(currentTime/60)) + ' : ' + ((~~(currentTime%60)) < 10 ? ('0'+~~(currentTime%60)) : ~~(currentTime%60)));
-            if(currentTime <= 10) { 
-                !$(target).hasClass('is-warning') && $(target).addClass('is-warning');
-            }else{
-                $(target).hasClass('is-warning') && $(target).removeClass('is-warning');
-            }
-            if(currentTime == 0) { clearInterval(stimer); setTimeout(()=>alert('로그아웃되었습니다.'),100) }
-        }
-        function stimerStart() { 
-            clearInterval(stimer);
-            stimer = setInterval(counter,1000); 
+    function dialog() { // 다이알로그 공통
+        const _this = document.querySelectorAll('.cp_dialog');
+
+        _this.forEach((cp,i)=>{
+            cp.querySelectorAll('.bt_dialogClose, .bt_cancel').forEach((el,j)=>{
+                if(!el.getAttribute('onclick')){
+                    el.addEventListener('click', (e)=>{
+                        e.preventDefault();
+                        cp.classList.contains('is-active') && cp.classList.remove('is-active');
+                    });
+                }
+            });
+        });
+    }
+    function dialogOpen(target) { // 다이알로그 열기 함수
+        !document.querySelector(target)?.classList.contains('is-active') && document.querySelector(target)?.classList.add('is-active');
+    }
+    function dialogClose(target) { // 다이알로그 닫기 함수
+        if(typeof(target) === 'string') {
+            document.querySelector(target)?.classList.contains('is-active') && document.querySelector(target)?.classList.remove('is-active');
+        }else {
+            target.closest('.cp_dialog')?.classList.contains('is-active') && target.closest('.cp_dialog')?.classList.remove('is-active');
         }
     }
 
-    function dialog() {}
+    function drag() {
+        const _this = document.querySelectorAll('.nm-webMessenger .contact dd');
+
+        _this.forEach((el,i)=>{
+            console.log('작업예정')
+        });
+    }
     
 
     return {
+        scroll,
         gnb,
         tab,
         sectionTimer,
-        dialog,
+        dialog, dialogOpen, dialogClose,
+        integratedSearch,
+        drag,
     }
 })(window, document, jQuery);
 
 // DOMContentLoaded
-$(function() {
-    // ui.gnb();
+// $(function() {
+//     // ui.gnb();
+// });
+window.addEventListener('DOMContentLoaded',()=>{
+
 });
 
-$(window).on('load', function(){
-    ui.gnb();
-    ui.tab();
-    // $('.cp_dialog').addClass('is-active');
+
+window.addEventListener('load', function(){
+    uiux.scroll();
+    uiux.gnb();
+    uiux.tab();
+    uiux.dialog();
+    uiux.integratedSearch();
+    uiux.drag();
+    // document.querySelector('#workRegister').classList.add('is-active');
 });
 
 
@@ -326,7 +374,7 @@ $(window).on('load', function(){
 // text
 function alarm() {
     // $('.sectionTimer').toggleClass('is-warning');
-    // ui.sectionTimer('.sectionTimer',10);
-    ui.sectionTimer(14,true);
+    // uiux.sectionTimer('.sectionTimer',10);
+    uiux.sectionTimer(14,true);
     
 }
